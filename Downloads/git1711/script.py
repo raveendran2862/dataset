e29457beb6a976d7a2b8533fcfbc3bf6382be287
Rahul <Rahul123@gmail.com>
2025-11-17 10:02:26 +0530

    Its basic data

---
+++ Downloads/git1711/script.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load cleaned dataset
+df = pd.read_csv("https://raw.githubusercontent.com/raveendran2862/dataset/refs/heads/main/marketing_customer_data_uncleaned.csv")
+print(df)
