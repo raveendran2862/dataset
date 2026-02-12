bedb5b1415420e2a7f6f319b58d65e1d5d4658d8
Rahul <Rahul123@gmail.com>
2026-02-12 10:16:44 +0530

    pandas file

---
+++ Downloads/git1202/script.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
+data = {"Name":["Sakthi","yuvan","vinesh","Hari"],
+	"Age":[21,22,23,24],
+	"Gender":["M","M","M","M"]}
+
+
+df = pd.DataFrame(data)
+print(df)
